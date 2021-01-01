c423323e536d9a5a2e8ee5599d7364025087806d
marianos <marianos8125@gmail.com>
2020-12-31 23:08:03 -0300

    display list of recipes on index page

--- src/main/java/guru/springframework/repositories/CategoryRepository.java
+++ src/main/java/guru/springframework/repositories/CategoryRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
-public interface CategoryRepository extends CrudRepository<guru.springframework.domain.Category, Long> {
+public interface CategoryRepository extends CrudRepository<Category, Long> {
 
     Optional<Category> findByDescription(String description);
 }
