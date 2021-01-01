9912f244d2e746462651fd392004e5fc450bf994
marianos <marianos8125@gmail.com>
2021-01-01 18:37:22 -0300

    using setters for jpa bidirectional relationships

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -117,9 +117,16 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+        notes.setRecipe(this);
     }
 
-    public Set<Ingredient> getIngredients() {
+    public Recipe addIngredient(Ingredient ingredient){
+        ingredient.setRecipe(this);
+        this.ingredients.add(ingredient);
+        return this;
+    }
+
+    public Set<Ingredient> addIngredient() {
         return ingredients;
     }
 
